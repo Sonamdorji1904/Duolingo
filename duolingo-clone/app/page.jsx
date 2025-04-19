510549e823438b4b8fc341bbe24668713e7cc773
Sonamdorji1904 <02240362.cst@rub.edu.bt>
2025-04-19 15:59:18 +0600

    feat: created a web app for duolingo

---
+++ duolingo-clone/app/page.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "@/components/Sidebar";
+import MainContent from "@/components/MainContent";
+import RightSidebar from "@/components/RightSidebar";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen font-sans bg-white">
+      <Sidebar />
+      <MainContent />
+      <RightSidebar />
+    </main>
+  );
+}
