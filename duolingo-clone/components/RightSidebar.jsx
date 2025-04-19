969164ca34d9cda2c225c40a299de647aeb1b69d
Sonamdorji1904 <02240362.cst@rub.edu.bt>
2025-04-19 18:50:01 +0600

    feat(lesson): add lesson UI layout

---
+++ duolingo-clone/components/RightSidebar.jsx
@@ -0,0 +1,8 @@
+export default function RightSidebar() {
+    return (
+      <aside className="w-80 border-l border-gray-200 bg-white p-4">
+        <h2 className="text-xl font-semibold mb-4">Right Sidebar</h2>
+        <p>Daily XP tracker, quests, leaderboard, etc. go here.</p>
+      </aside>
+    );
+  }
