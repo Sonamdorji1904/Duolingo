bfd1138c7f77ca5c57e458c80714e46dd2022f71
Sonamdorji1904 <02240362.cst@rub.edu.bt>
2025-04-20 15:37:42 +0600

    feat(letters): updated layout structure

--- duolingo-clone/app/layout.js
+++ duolingo-clone/app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Sidebar from "@/components/Sidebar"; // Adjust the path if needed
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,8 +22,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Sidebar />
-        <main className="ml-60 p-6">{children}</main>
+        {children}
       </body>
     </html>
   );
